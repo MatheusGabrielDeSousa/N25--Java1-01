c1e3c0d1f73503ca954c42e5883a03ac62f333c0
Matheus Gabriel de Sousa <matheusgabrields20@gmail.com>
2025-06-09 21:54:28 -0300

    Greenfoot

---
+++ Greenfoot/MundoAbelha/BeeWorld.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Definição da Classe BeeWorld.
+ * 
+ * @author Richard Brosler 
+ * @version 2025-06-04
+ */
+public class BeeWorld extends World
+{
+
+    /**
+     * Constructor for objects of class BeeWorld.
+     * 
+     */
+    public BeeWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Aranha aranha = new Aranha();
+        addObject(aranha,32,33);
+
+        Abelha abelha = new Abelha();
+        addObject(abelha,45,152);
+
+        for (int qtd=0; qtd<20; qtd++){
+            int posX = Greenfoot.getRandomNumber(800);
+            int posY = Greenfoot.getRandomNumber(600);
+            int vel = Greenfoot.getRandomNumber(3)+1;
+            int rot= Greenfoot.getRandomNumber(360)+1;
+            addObject(new Mosca(vel,rot), posX, posY);
+        }
+    }
+}
---
+++ Greenfoot/MundoAbelha/Mosca.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Aqui é a Classe Mosca.
+ * 
+ * @author Richard Brosler
+ * @version 2025-06-03
+ */
+public class Mosca extends Inseto
+{
+    //Fields
+    private int velocidade;
+    private int rotacao;
+    //Constructor padrão
+    public Mosca(){
+        velocidade = 1;
+        rotacao = 0;
+    }
+        //Constructor Mosca com parametros
+    public Mosca(int vel,int rot){
+            velocidade = vel;
+            rotacao = rot;
+            setRotation(rot);
+        }
+    /**
+    ** Act - Método responsável pelas ações da mosca quando
+    * os botões Act ou Run são pressionados */public void act()
+    {
+        // Add your action code here.
+        move(1);
+        if (Greenfoot.getRandomNumber(100)<20){
+            turn(45-Greenfoot.getRandomNumber(90));
+        }
+        verificarCanto();
+    }
+}
